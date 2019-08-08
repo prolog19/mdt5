18ee851e0377941f0514cce04949efe8c04252bf
Усатов Дмитрий Игоревич <dusatov@PHOENIXIT.RU>
2019-08-08 17:26:12 +0300

    Learn HTML5 and start to learn CSS3

--- code.js
+++ code.js
@@ -35,14 +35,10 @@
         }
     });
 
-    var app5 = new Vue({
-        el: '#app-5',
+
+    var app6 = new Vue({
+        el: '#app-6',
         data: {
-            message: 'Привет, Vue.js!'
-        },
-        methods: {
-            reverseMessage: function() {
-                this.message = this.message.split('').reverse().join('')
-            }
+            message: 'Привет, Vue!'
         }
     });
